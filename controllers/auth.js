a1ad2c1d53526d49170cdec2ef1f0af4471b5e49
sarto007 <alhazucloud@gmail.com>
2025-01-20 14:27:58 +0000

    m

--- controllers/auth.js
+++ controllers/auth.js
@@ -59,20 +59,17 @@ console.log("user created!")
 
 //Login
 
-export const login= async(req,res,next)=>{
-  try{
-const user= await User.findOne({username:req.body.username})
-if(!user) return next(createError(404, "user not found"));
-const isPasswordCorrect= await bcrypt.compare(req.body.password, user.password)
-if(!isPasswordCorrect) return next(createError(400, "wrong password or username"));
-const token= jwt.sign({id:user._id, isAdmin: user.isAdmin}, "#@%itsx#$%trong")
-
-const {password, isAdmin, ...otherDetails}=user._doc
-res.cookie("access_token",token,{
-  httpOnly:true
-}).status(200).json({details:{...otherDetails},isAdmin})
-
-  }catch(err){
-    next(err)
-  }
-}
+export const login = async (req, res) => { 
+  const { username, password } = req.body; 
+  try { const user = await User.findOne({ username }); 
+  if (!user) { return res.status(404).json({ message: 'User not found' });
+ } 
+ const isPasswordValid = await bcrypt.compare(password, user.password); 
+ if (!isPasswordValid) { return res.status(400).json({ message: 'Invalid credentials' }); 
+} 
+const token = jwt.sign({ id: user._id }, process.env.JWT, { expiresIn: '1h', }); 
+res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production', });
+ res.status(200).json({ message: 'Login successful' }); 
+} catch (error){
+     res.status(500).json({ message: 'Server error' });
+ } };
