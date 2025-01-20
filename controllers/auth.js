72e77766c0043d7e62de1ea33276ac90a267ed4c
sarto007 <alhazucloud@gmail.com>
2025-01-20 15:15:18 +0000

    m

--- controllers/auth.js
+++ controllers/auth.js
@@ -67,10 +67,10 @@ const user= await User.findOne({username:req.body.username})
 if(!user) return next(createError(404, "user not found"));
 const isPasswordCorrect= await bcrypt.compare(req.body.password, user.password)
 if(!isPasswordCorrect) return next(createError(400, "wrong password or username"));
-const token= jwt.sign({id:user._id, isAdmin: user.isAdmin}, "#@%itsx#$%trong")
+const token= jwt.sign({id:user._id, isAdmin: user.isAdmin}, process.env.JWT)
 
 const {password, isAdmin, ...otherDetails}=user._doc
-res.cookie("access_token",token,{
+res.cookie("access_token",token, {
   httpOnly:true
 }).status(200).json({details:{...otherDetails},isAdmin})
 
