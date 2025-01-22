89113e92a525ae866c2c2328b5f2e7ec653899c9
sarto007 <alhazucloud@gmail.com>
2025-01-22 13:59:10 +0000

    m

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import {register,createUser, login, AdminLogin} from '../controllers/auth.js'
+import {register,createUser, login, UserLogin} from '../controllers/auth.js'
 import {post} from '../controllers/posts.js'
 
 import {searchPost} from '../controllers/searchPost.js'
@@ -12,7 +12,7 @@ const router = express.Router();
 router.post('/login', login); 
 router.post('/sign_up', createUser); 
 router.post('/posts', post);
-router.post('/adminLogin',AdminLogin)
+router.post('/userLogin',UserLogin)
 
 
 
