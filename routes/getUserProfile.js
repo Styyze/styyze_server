61f16dd511c42cd9db6b58844c4921896c702df6
sarto007 <alhazucloud@gmail.com>
2025-04-19 15:06:32 +0000

    added

---
+++ routes/getUserProfile.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+import {getUserProfile} from '../controllers/userProfile.js'
+
+
+
+
+const router = express.Router();
+
+
+router.get('/getUserProfile/:userId', getUserProfile);
+
+
+export default router;
