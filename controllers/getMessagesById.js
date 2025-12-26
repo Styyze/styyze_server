05a1eb74c6413d76cef6fc1d9f8d85d86094d55b
sarto007 <alhazucloud@gmail.com>
2025-12-26 20:12:01 +0000

    debugged

--- controllers/getMessagesById.js
+++ controllers/getMessagesById.js
@@ -9,17 +9,17 @@ export const getUserMessagesById = async (req, res, next) => {
        return  res.status(400).json({success: false, message: "ConversationId is Required"})
     }
     const messages = await Message.find({conversationId:conversationId})
-    .sort({timestamps: -1 });
-console.log("collection name:", Message.collection.name);
+    .sort({createdAt: -1 });
+
     if (messages.length === 0){
         return res.status(404).json({success: false, message: "No message for this user"});
     }
-    return res.status(200).json({success: true, data:messagesu});
+    return res.status(200).json({success: true, data:messages});
   }catch(err){
 console.log(' error:', err);
 return res.status(500).json({
     success:false,
-    message: "Server Error"
+    message: err.message
 })
   }
 }
