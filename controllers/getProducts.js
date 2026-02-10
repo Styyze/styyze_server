ea25185578592f158924615a6ba6db0ff09a2770
sarto007 <alhazucloud@gmail.com>
2026-02-10 11:53:31 +0000

    updated stock

--- controllers/getProducts.js
+++ controllers/getProducts.js
@@ -4,60 +4,57 @@ import UserProfile from '../models/UserProfile.js';
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.aggregate([
-      {
-        $match: {
-    stock: { $gt: 0 }
-           }
-      },
 
-      //  Join with UserProfile using sellerId → userId
-      {
-        $lookup: {
-          from: 'userprofiles', 
-          localField: 'seller',
-          foreignField: 'userId',
-          as: 'sellerProfile'
-        }
-      },
+  // Join with UserProfile using seller → userId
+  {
+    $lookup: {
+      from: 'userprofiles',
+      localField: 'seller',
+      foreignField: 'userId',
+      as: 'sellerProfile'
+    }
+  },
 
-      //  Convert sellerProfile array → object
-      {
-        $unwind: {
-          path: '$sellerProfile',
-          preserveNullAndEmptyArrays: true
-        }
-      },
+  // Convert sellerProfile array → object
+  {
+    $unwind: {
+      path: '$sellerProfile',
+      preserveNullAndEmptyArrays: true
+    }
+  },
 
-      // Shape the response
-      {
-        $project: {
-          title: 1,
-          description: 1,
-          price: 1,
-          currency: 1,
-          size: 1,
-          color: 1,
-          category: 1,
-          stock: 1,
-          media: 1,
-          createdAt: 1,
+  // Shape response
+  {
+    $project: {
+      title: 1,
+      description: 1,
+      price: 1,
+      currency: 1,
+      size: 1,
+      color: 1,
+      category: 1,
+      stock: 1,
+      media: 1,
+      createdAt: 1,
 
-          seller: {
-            userId: '$sellerProfile.userId',
-            name: '$sellerProfile.name',
-            username: '$sellerProfile.username',
-            avatarUrl: '$sellerProfile.avatarUrl',
-            bio: '$sellerProfile.bio',
-            location: '$sellerProfile.location',
-          }
-        }
-      },
-
-      //  Latest products first
-      {
-        $sort: { createdAt: -1 }
+      seller: {
+        userId: '$sellerProfile.userId',
+        name: '$sellerProfile.name',
+        username: '$sellerProfile.username',
+        avatarUrl: '$sellerProfile.avatarUrl',
+        bio: '$sellerProfile.bio',
+        location: '$sellerProfile.location',
       }
-    ]);
+    }
+  },
+
+  // Latest products first
+  {
+    $sort: { createdAt: -1 }
+  }
+
+]);
+
 
     return res.status(200).json({
       success: true,
