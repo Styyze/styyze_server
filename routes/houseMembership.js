16ba25a3ad80fb5c780fd1ce17b696c5cf5214ad
sarto007 <alhazucloud@gmail.com>
2026-07-26 18:15:46 +0000

    get invitation

--- routes/houseMembership.js
+++ routes/houseMembership.js
@@ -9,7 +9,7 @@ import { protect } from '../middleware/auth.js';
 const router = express.Router();
 
 router.post("/houses/create", protect, createHouse);
-router.get("/houses/staff/invitation", getInvitations);
+router.get("/houses/staff/invitation", protect, getInvitations);
 router.post( "/houses/staff/invite", protect, inviteStaff);
 
 
