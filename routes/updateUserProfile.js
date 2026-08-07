7e5d8b5701cacebfe29888f15fa874160d408c9a
sarto007 <alhazucloud@gmail.com>
2026-08-07 19:55:56 +0000

    protect

--- routes/updateUserProfile.js
+++ routes/updateUserProfile.js
@@ -1,11 +1,11 @@
 import express from 'express'
 
 import {updateUserProfile} from '../controllers/userProfile.js';
-import {authenticate} from '../controllers/authmiddleware.js';
+import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.patch('/updateUserProfile', authenticate, updateUserProfile)
+router.patch('/updateUserProfile', protect, updateUserProfile)
 
 
 export default router;
