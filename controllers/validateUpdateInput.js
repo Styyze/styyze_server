ec04bcc84861e8e439cb2b89b6b0b6fa3f10c968
sarto007 <alhazucloud@gmail.com>
2026-08-08 15:10:59 +0000

    bio error4

--- controllers/validateUpdateInput.js
+++ controllers/validateUpdateInput.js
@@ -1,6 +1,5 @@
 import Joi from "joi";
 
-
 export const validateUpdateInput = (data) => {
     const mediaItemSchema = Joi.object({
         mediaUrl: Joi.string().uri().optional(),
@@ -12,11 +11,14 @@ export const validateUpdateInput = (data) => {
         name: Joi.string().max(100).optional(),
         username: Joi.string().alphanum().min(3).max(30).optional(),
         
+        // Add bio here!
+        bio: Joi.string().max(500).allow('').optional(),
+        
         avatarUrl: Joi.array().items(mediaItemSchema).optional(),
         coverPhotoUrl: Joi.array().items(mediaItemSchema).optional(),
         
-        location: Joi.string().max(100).optional(),
-        website: Joi.string().uri().optional()
+        location: Joi.string().max(100).allow('').optional(),
+        website: Joi.string().uri().allow('').optional()
     });
     
     return schema.validate(data);
