63ec89c2ca7987eed1e4a696dbe2ac5ac02ab4ed
sarto007 <alhazucloud@gmail.com>
2026-08-08 14:50:50 +0000

    bio error3

--- controllers/userProfile.js
+++ controllers/userProfile.js
@@ -96,7 +96,10 @@ export const updateUserProfile = async (req, res, next) => {
 
         // Input validation with Joi
         const { error } = validateUpdateInput(req.body);
+
         if (error) {
+            console.log("bio error", error.details[0].message );
+
             return res.status(400).json({ 
                 success: false, 
                 message: error.details[0].message 
--- controllers/validateUpdateInput.js
+++ controllers/validateUpdateInput.js
@@ -11,7 +11,6 @@ export const validateUpdateInput = (data) => {
         userId: Joi.string().required(), 
         name: Joi.string().max(100).optional(),
         username: Joi.string().alphanum().min(3).max(30).optional(),
-        bio: Joi.string().max(500).optional(),
         
         avatarUrl: Joi.array().items(mediaItemSchema).optional(),
         coverPhotoUrl: Joi.array().items(mediaItemSchema).optional(),
