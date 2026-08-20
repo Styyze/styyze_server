708998cd9565e3a4a697bb05d77c64b4368038ff
sarto007 <alhazucloud@gmail.com>
2026-08-20 20:50:02 +0000

    get house projects2

--- routes/project.js
+++ routes/project.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { protect } from '../middleware/auth.js';
 
-import {createProject } from '../controllers/project.js';
+import {createProject, getHouseProjects } from '../controllers/project.js';
 
 const router = express.Router();
 
 
 router.post('/projects', protect, createProject);
+router.get('/get/house/projects', getHouseProjects);
 
 export default router
